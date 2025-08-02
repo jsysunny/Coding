8658e9b0d1db8a23dd8ae3044e3d56f68634ef49
jsysunny <sunny011009.jsy@gmail.com>
2025-08-02 10:56:59 +0900

    Title: 나눗셈의 나머지, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250802/나눗셈의 나머지/remainder-of-division.py
+++ 250802/나눗셈의 나머지/remainder-of-division.py
@@ -1,13 +1,17 @@
 a,b = map(int, input().split())
-cnt=[0]*9
+cnt=[0]*b
 while a>1:
     c=a%b
+    #print(c)
     cnt[c]+=1
+    #print(cnt)
     a=a//b
 
 
 suma=0
 for c in cnt:
+    #print(c)
     suma+=c**2
+    #print(suma)
 
 print(suma)
