bd043c69d0a1a893f5c696374fd0c92f52caecfb
jsysunny <sunny011009.jsy@gmail.com>
2025-08-09 14:22:05 +0900

    Title: 함수를 이용한 369 게임, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250809/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+def A(a,b):
+    cnt=0
+    for i in range(a,b+1):
+        if i//10==3 or i //10==6 or i//10==9 or i%10==3 or  i%10==6 or i%10==9 or i%3==0:
+            cnt+=1
+    return cnt
+
+print(A(a,b))
