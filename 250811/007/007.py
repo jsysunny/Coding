25a686c732d8146d1da5c004d22c6bfd323cb2da
jsysunny <sunny011009.jsy@gmail.com>
2025-08-11 11:34:09 +0900

    Title: 007, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250811/007/007.py
+++ 250811/007/007.py
@@ -2,5 +2,6 @@ secret_code, meeting_point, time = input().split()
 time = int(time)
 
 # Please write your code here.
-print(f"secret_code : {secret_code})
-print(f"secret_code : {secret_code})
+print(f"secret code : {secret_code}")
+print(f"meeting point : {meeting_point}")
+print(f"time : {time}")
