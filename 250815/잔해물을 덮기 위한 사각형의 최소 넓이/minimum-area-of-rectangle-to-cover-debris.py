4a012c3aaa7a5d79f00720ee4b3cc315c79356e2
jsysunny <sunny011009.jsy@gmail.com>
2025-08-15 13:39:40 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 71ms, Memory: 46MB, Status: Wrong Answer - Codetree

--- 250815/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250815/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -13,27 +13,25 @@ for k in range(len(x1)):
                 placed[i][j]-=1
 
 cnt=[]
+height=width=0
 for i in range(x1[0],x2[0]):
     cnt1=0
     for j in range(y1[0],y2[0]):
         if placed[i][j]:
             cnt1+=1
-    if cnt1==y2[0]-y1[0]:
-        cnt.append(cnt1)
-        break
-if not cnt:
-    cnt.append(cnt1)
+    if cnt1>0:
+        height= max(height, cnt1)
+cnt.append(height)
+
 
 for j in range(y1[0],y2[0]):
     cnt2=0
     for i in range(x1[0],x2[0]):
         if placed[i][j]:
             cnt2+=1
-    if cnt2==x2[0]-x1[0]:
-        cnt.append(cnt2)
-        break
-if len(cnt)==1:
-    cnt.append(cnt2)
+    if cnt2>0:
+        width=max(width,cnt2)
+cnt.append(width)
 
 print(cnt[0]*cnt[1])
 
