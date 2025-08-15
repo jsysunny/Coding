cf6a5b5cb59603939990962ebdf89e0cdac650dc
jsysunny <sunny011009.jsy@gmail.com>
2025-08-15 18:48:11 +0900

    Title: 벌금은 누구에게, Time: 77ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250815/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,11 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+stuset=[0]*N
+for i in range(M):
+    stuset[student[i]-1]+=1
+
+for i in range(N):
+    if stuset[i]==K:
+        print(i+1)
