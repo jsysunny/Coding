40d96c85b9bc3fc51828f6370ad346041e26165a
jsysunny <sunny011009.jsy@gmail.com>
2025-08-16 09:58:01 +0900

    Title: 선두를 지켜라, Time: 97ms, Memory: 24MB, Status: Runtime Error - Codetree

---
+++ 250816/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,50 @@
+n, m = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(n):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(m):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
+posa=[0]*20000 ; posb=[0]*20000 ;
+timea=1 ; timeb=1 ;
+for i in range(n):
+    for j in range(t[i]):
+        #print(v[i])
+        posa[timea]=posa[timea-1]+v[i]
+        #print(posa[timea])
+        timea+=1
+
+for i in range(m):
+    for j in range(t2[i]):
+        posb[timeb]=posb[timeb-1]+v2[i]
+        timeb+=1
+
+#print(posa)
+afirst=True
+
+if posa[1]<posb[1]:
+    afirst=False
+#print(afirst)
+cnt=0
+for i in range(2,20000):
+    if afirst and posa[i]<posb[i]:
+        #print(1)
+        afirst=False
+        cnt+=1
+    elif not afirst and posa[i]>posb[i]:
+        #print(2)
+        afirst=True
+        cnt+=1
+print(cnt)
