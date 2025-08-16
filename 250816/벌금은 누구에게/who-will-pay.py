534ebc9e5790f9de39516d2b052e30b2f8e17a35
jsysunny <sunny011009.jsy@gmail.com>
2025-08-16 10:14:39 +0900

    Title: 벌금은 누구에게, Time: 92ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250816/벌금은 누구에게/who-will-pay.py
+++ 250816/벌금은 누구에게/who-will-pay.py
@@ -4,12 +4,12 @@ student = [int(input()) for _ in range(M)]
 #print(student)
 # Please write your code here.
 stuset=[0]*N
+ans=-1
 for i in range(M):
     stuset[student[i]-1]+=1
+    for i in range(N):
+        if stuset[i]>=K:
+            ans= i+1
+            break
 
-ans=-1
-for i in range(N):
-    if stuset[i]>=K:
-        ans= i+1
-        break
 print(ans)
