4eaf8a588db10150ebceb60360a0b7b793d3f674
jsysunny <sunny011009.jsy@gmail.com>
2025-08-17 09:46:59 +0900

    Title: 코드네임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250817/코드네임/code-name.py
@@ -0,0 +1,25 @@
+MAX_N = 5
+
+codenames = []
+scores = []
+for _ in range(MAX_N):
+    codename, score = input().split()
+    codenames.append(codename)
+    scores.append(int(score))
+
+class Name:
+    def __init__(self, codename, score):
+        self. codename= codename
+        self.score=score
+
+name=[]
+# Please write your code here.
+for i in range(len(codenames)):
+    name.append(Name(codenames[i], scores[i]))
+
+mina=100
+for i in range(len(name)):
+    if name[i].score < mina:
+        mina= name[i].score
+    else:
+        print(name[i].codename, name[i].score)
