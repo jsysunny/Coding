e0d031603e7d9b0bcc5dd643eb5a925c9ff0b024
jsysunny <sunny011009.jsy@gmail.com>
2025-10-29 16:11:37 +0900

    Title: 일렬로 서있는 소 2, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251029/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,12 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+
+cnt=0
+for i in range(len(A)):
+    for j in range(i+1, len(A)):
+        for k in range(j+1, len(A)):
+            if A[i]<= A[j] and A[j]<=A[k]:
+                cnt+=1
+print(cnt)
