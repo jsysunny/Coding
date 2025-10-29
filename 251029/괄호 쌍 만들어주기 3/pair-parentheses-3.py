ff43633818d7a02014834e3005fb481ed3be4f7a
jsysunny <sunny011009.jsy@gmail.com>
2025-10-29 16:06:03 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251029/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,11 @@
+A = input()
+A= list(A)
+# Please write your code here.
+
+cnt=0
+for i in range(len(A)):
+    for j in range(i+1, len(A)):
+        if A[i]=="(" and A[j]==")":
+            cnt+=1
+
+print(cnt)
