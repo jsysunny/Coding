131c59a46c56196800a4ec0ad38d0b1336dfbe87
jsysunny <sunny011009.jsy@gmail.com>
2025-10-29 21:24:17 +0900

    Title: 이상한 진수 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251029/이상한 진수 2/awkward-digits-2.py
+++ 251029/이상한 진수 2/awkward-digits-2.py
@@ -1,11 +1,14 @@
 a = input()
 a=list(a)
 # Please write your code here.
-
+flag= False
 for i in range(len(a)):
     if a[i]=='0':
+        flag=True
         a[i]='1'
         break
+if not flag:
+    a[len(a)-1]='0'
 
 suma=0
 for i in range(len(a)):
@@ -13,3 +16,7 @@ for i in range(len(a)):
     suma += 2**(len(a)-1-i) * int(a[i])
     #print("합",2**(len(a)-1-i))
 print(suma)
+
+# 이진수 → 10진수 변환
+#ans = int(''.join(a), 2)
+#print(ans)
