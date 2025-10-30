83f543c7899b7f3f21d09dc397e2ce50cd437f0f
jsysunny <sunny011009.jsy@gmail.com>
2025-10-30 11:41:29 +0900

    Title: G or H 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251030/G or H 3/G-or-H-3.py
+++ 251030/G or H 3/G-or-H-3.py
@@ -6,31 +6,28 @@ for _ in range(n):
     x.append(int(pos))
     c.append(char)
 
-posa=[0]*10001
-posb=[0]*10001
+#print(max(x))
+posa=[0]*(max(x)+1)
+posb=[0]*(max(x)+1)
 # Please write your code here.
-for i in range(k):
+
+for i in range(len(x)):
     posa[x[i]]=1
     posb[x[i]]=c[i]
 
 
 maxa=0
-for i in range(10000-k+1):
+for i in range(1,max(x)-k+1):
     suma=0
     for j in range(i,i+k+1):
         if posa[j]==1:
             if posb[j]=="G":
-
                 suma+=1
                 #print(i,j,suma)
             elif posb[j]=="H":
-
                 suma+=2
                 #print(i,j,suma)
-            else:
-                continue
         else:
             continue
     maxa= max(maxa,suma)
 print(maxa)
-        
