791c3d78f44a065a4cca64bdef633d77763fad0c
jsysunny <sunny011009.jsy@gmail.com>
2025-11-03 17:55:54 +0900

    Title: 데이터센터의 온도 조정 2, Time: 90ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 251103/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,25 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+
+
+
+
+def a(num,Ta,Tb,C,H,G):
+    if num <Ta :
+        return C
+    elif num < Tb:
+        return G
+    else:
+        return H
+
+maxa=0
+for num in range(0,1000):
+    suma=0
+    for i in range(N):
+        Ta,Tb=ranges[i]
+        suma+= a(num,Ta,Tb,C,H,G)
+    maxa=max(maxa,suma)
+
+
+print(maxa)
+
