479dce5d745dd3654d8b731eb0a87b6b283ffb4d
jsysunny <sunny011009.jsy@gmail.com>
2025-11-03 17:12:49 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251103/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 251103/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -3,16 +3,13 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-minc=float("inf")
 mind=float("inf")
 # Please write your code here.
 for i in range(n):
-    mina=float("inf")
-    minb=float("inf")
     for j in range(i+1,n):
-        mina= min (mina, abs(points[i][0]- points[j][0]))
-        minb= min (minb, abs(points[i][1]- points[j][1]))
-    minc= min(minc,mina)
-    mind= min(mind,minb)
+        dx = points[i][0] - points[j][0]
+        dy = points[i][1] - points[j][1]
+        dist = dx * dx + dy * dy
+        mind = min(mind, dist)
 
-print(minc**2+ mind**2)
+print(mind)
