75bfa0bf2df83896fe8069097c4d517bf38fe956
jsysunny <sunny011009.jsy@gmail.com>
2025-11-04 17:51:54 +0900

    Title: 수들의 최대 차, Time: 128ms, Memory: 17MB, Status: Passed - Codetree

--- 251104/수들의 최대 차/maximum-difference-in-numbers.py
+++ 251104/수들의 최대 차/maximum-difference-in-numbers.py
@@ -2,15 +2,11 @@ N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 
 # Please write your code here.
-minc=float("inf")
+maxc=0
 for i in range(10000):
     cost=0
     for a in arr:
-        if a>=i and a<=i+2:
-            continue
-        elif a<i:
-            cost+= i-a
-        else:
-            cost+= a-(i+2)
-    minc= min(minc, cost)
-print(minc)
+        if a>=i and a<=i+K:
+            cost+=1
+    maxc= max(maxc, cost)
+print(maxc)
